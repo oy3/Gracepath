2d498a0beca3b709ba02406e59ec77216c69d800
oy3 <46595215+oy3@users.noreply.github.com>
2021-07-09 13:09:52 +0100

    Version 2.2: Added contact us code to index.php

--- index.php
+++ index.php
@@ -30,10 +30,13 @@
    <link rel="stylesheet" href="css/custom.css">
    <!-- Modernizer for Portfolio -->
    <script src="js/modernizer.js"></script>
-   <!-- [if lt IE 9] -->
+   <!-- Alertify.JS -->
+   <link rel="stylesheet" href="alertifyjs/css/alertify.min.css" />
+   <link rel="stylesheet" href="alertifyjs/css/themes/default.min.css" />
 </head>
 
 <body class="clinic_version">
+   <script src="alertifyjs/alertify.min.js"></script>
    <!-- LOADER -->
    <div id="preloader">
       <img class="" src="images/loaders/66.gif" alt="">
@@ -42,7 +45,7 @@
    <header>
       <div class="header-top wow fadeIn">
          <div class="container">
-            <a class="navbar-brand" href="index.html"><img src="images/gracepath.png" alt="image"></a>
+            <a class="navbar-brand" href="index.php"><img src="images/gracepath.png" alt="image"></a>
             <div class="right-header">
                <div class="header-info">
                   <div class="info-inner">
@@ -65,13 +68,12 @@
          <div class="container">
             <nav class="main-menu">
                <div class="navbar-header">
-                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
-                     aria-expanded="false" aria-controls="navbar"><i class="fa fa-bars" aria-hidden="true"></i></button>
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i class="fa fa-bars" aria-hidden="true"></i></button>
                </div>
 
                <div id="navbar" class="navbar-collapse collapse">
                   <ul class="nav navbar-nav">
-                     <li><a class="active" href="index.html">Home</a></li>
+                     <li><a class="active" href="index.php">Home</a></li>
                      <li><a href="doctors.html">Doctors</a></li>
                      <!-- <li><a href="">Book Appointment</a></li> Book Appointment page link goes here -->
                      <li><a href="about.html">About us</a></li>
@@ -110,17 +112,14 @@
       </div>
    </header>
 
-   <div id="home" class="parallax first-section wow fadeIn" data-stellar-background-ratio="0.4"
-      style="background-image:url('images/slider-bg.png');">
+   <div id="home" class="parallax first-section wow fadeIn" data-stellar-background-ratio="0.4" style="background-image:url('images/slider-bg.png');">
       <div class="container">
          <div class="row">
             <div class="col-md-12 col-sm-12">
                <div class="text-contant">
                   <h2>
-                     <span class="center"><span class="icon"><img src="images/gracepath-icon.png"
-                              alt="#" /></span></span>
-                     <a href="" class="typewrite" data-period="2000"
-                        data-type='[ "Welcome to Grace Path Diagnostic Centre", "We Care Your Health", "We are Expert!" ]'>
+                     <span class="center"><span class="icon"><img src="images/gracepath-icon.png" alt="#" /></span></span>
+                     <a href="" class="typewrite" data-period="2000" data-type='[ "Welcome to Grace Path Diagnostic Centre", "We Care Your Health", "We are Expert!" ]'>
                         <span class="wrap"></span>
                      </a>
                   </h2>
@@ -197,8 +196,7 @@
             <div class="col-md-6">
                <div class="post-media wow fadeIn">
                   <img src="images/about_03.jpg" alt="" class="img-responsive">
-                  <a href="https://www.youtube.com/watch?v=OLIRjW_1eOQ" data-rel="prettyPhoto[gal]" class="playbutton"><i
-                        class="flaticon-play-button"></i></a>
+                  <a href="https://www.youtube.com/watch?v=OLIRjW_1eOQ" data-rel="prettyPhoto[gal]" class="playbutton"><i class="flaticon-play-button"></i></a>
                </div>
                <!-- end media -->
             </div>
@@ -210,8 +208,7 @@
             <div class="col-md-3 col-sm-6 col-xs-12">
                <div class="service-widget">
                   <div class="post-media wow fadeIn">
-                     <a href="images/clinic_04.jpg" data-rel="prettyPhoto[gal]" class="hoverbutton global-radius"><i
-                           class="flaticon-unlink"></i></a>
+                     <a href="images/clinic_04.jpg" data-rel="prettyPhoto[gal]" class="hoverbutton global-radius"><i class="flaticon-unlink"></i></a>
                      <img src="images/clinic_04.jpg" alt="" class="img-responsive">
                   </div>
                   <h3 style="font-size: 90%;">A spacious purpose 3-Story building</h3>
@@ -221,8 +218,7 @@
             <div class="col-md-3 col-sm-6 col-xs-12">
                <div class="service-widget">
                   <div class="post-media wow fadeIn">
-                     <a href="images/clinic_01.jpg" data-rel="prettyPhoto[gal]" class="hoverbutton global-radius"><i
-                           class="flaticon-unlink"></i></a>
+                     <a href="images/clinic_01.jpg" data-rel="prettyPhoto[gal]" class="hoverbutton global-radius"><i class="flaticon-unlink"></i></a>
                      <img src="images/clinic_01.jpg" alt="" class="img-responsive">
                   </div>
                   <h3>Detailed Medical Check-up</h3>
@@ -232,8 +228,7 @@
             <div class="col-md-3 col-sm-6 col-xs-12">
                <div class="service-widget">
                   <div class="post-media wow fadeIn">
-                     <a href="images/clinic_03.jpg" data-rel="prettyPhoto[gal]" class="hoverbutton global-radius"><i
-                           class="flaticon-unlink"></i></a>
+                     <a href="images/clinic_03.jpg" data-rel="prettyPhoto[gal]" class="hoverbutton global-radius"><i class="flaticon-unlink"></i></a>
                      <img src="images/clinic_03.jpg" alt="" class="img-responsive">
                   </div>
                   <h3>Specialist Physicians</h3>
@@ -243,8 +238,7 @@
             <div class="col-md-3 col-sm-6 col-xs-12">
                <div class="service-widget">
                   <div class="post-media wow fadeIn">
-                     <a href="images/clinic_02.jpg" data-rel="prettyPhoto[gal]" class="hoverbutton global-radius"><i
-                           class="flaticon-unlink"></i></a>
+                     <a href="images/clinic_02.jpg" data-rel="prettyPhoto[gal]" class="hoverbutton global-radius"><i class="flaticon-unlink"></i></a>
                      <img src="images/clinic_02.jpg" alt="" class="img-responsive">
                   </div>
                   <!-- <h3>Digital Control Center</h3> -->
@@ -315,7 +309,7 @@
                <div class="appointment-form">
                   <h3><span>+</span> Book Appointment</h3>
                   <div class="form">
-                     <form action="index.html">
+                     <form action="">
                         <fieldset>
                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                               <div class="row">
@@ -365,8 +359,7 @@
                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                               <div class="row">
                                  <div class="form-group">
-                                    <textarea rows="4" id="textarea_message" class="form-control"
-                                       placeholder="Your Message..."></textarea>
+                                    <textarea rows="4" id="textarea_message" class="form-control" placeholder="Your Message..."></textarea>
                                  </div>
                               </div>
                            </div>
@@ -388,8 +381,7 @@
       </div>
    </div>
 
-   <div id="doctors" class="parallax section db" data-stellar-background-ratio="0.4" style="background:#fff;"
-      data-scroll-id="doctors" tabindex="-1">
+   <div id="doctors" class="parallax section db" data-stellar-background-ratio="0.4" style="background:#fff;" data-scroll-id="doctors" tabindex="-1">
       <div class="container">
 
          <div class="heading">
@@ -398,8 +390,7 @@
          </div>
 
          <div class="row dev-list text-center">
-            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.2s"
-               style="visibility: visible; animation-duration: 1s; animation-delay: 0.2s; animation-name: fadeIn;">
+            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.2s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.2s; animation-name: fadeIn;">
                <div class="widget clearfix">
                   <img src="images/doctor_01.jpg" alt="" class="img-responsive img-rounded">
                   <div class="widget-title">
@@ -416,8 +407,7 @@
                </div>
             </div>
 
-            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.4s"
-               style="visibility: visible; animation-duration: 1s; animation-delay: 0.4s; animation-name: fadeIn;">
+            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.4s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.4s; animation-name: fadeIn;">
                <div class="widget clearfix">
                   <img src="images/doctor_02.jpg" alt="" class="img-responsive img-rounded">
                   <div class="widget-title">
@@ -475,8 +465,7 @@
       </div>
    </div>
 
-   <div id="price" class="section pr wow fadeIn"
-      style="background-image:url('images/price-bg.png'); margin-top: 100px; padding: 30px; overflow: visible;">
+   <div id="price" class="section pr wow fadeIn" style="background-image:url('images/price-bg.png'); margin-top: 100px; padding: 30px; overflow: visible;">
       <div class="container">
          <div class="row">
 
@@ -485,9 +474,9 @@
                <h1 style="color: white;"><i class="fa fa-phone-square"></i> +2484-398-8987</h1>
             </div>
             <div class="col-lg-4 col-md-4">
-               <a href=""> <!-- Book Appointment page link goes here {For Jude}-->
-                  <button type="button" class="btn btn-default btn-lg text-uppercase"
-                     style="-webkit-box-shadow: 4px 4px rgb(85 85 85 / 20%); -moz-box-shadow: 4px 4px rgba(85, 85, 85, 0.2); box-shadow: 4px 4px rgb(85 85 85 / 20%);">Book
+               <a href="">
+                  <!-- Book Appointment page link goes here {For Jude}-->
+                  <button type="button" class="btn btn-default btn-lg text-uppercase" style="-webkit-box-shadow: 4px 4px rgb(85 85 85 / 20%); -moz-box-shadow: 4px 4px rgba(85, 85, 85, 0.2); box-shadow: 4px 4px rgb(85 85 85 / 20%);">Book
                      your Appointment</button>
                </a>
             </div>
@@ -595,42 +584,77 @@
                <div class="row">
                   <div class="col-md-6">
                      <div class="form-group in_name">
-                        <input type="text" class="form-control" placeholder="Name" required="required">
+                        <input type="text" class="form-control" id="contact_name" placeholder="Name" required="required">
                      </div>
                   </div>
                   <div class="col-md-6">
                      <div class="form-group in_email">
-                        <input type="email" class="form-control" placeholder="E-mail" required="required">
+                        <input type="email" class="form-control" id="contact_email" placeholder="E-mail" required="required">
                      </div>
                   </div>
                   <div class="col-md-6">
                      <div class="form-group in_email">
-                        <input type="tel" class="form-control" id="phone" placeholder="Phone" required="required">
+                        <input type="tel" class="form-control" id="contact_phone" placeholder="Phone" required="required">
                      </div>
                   </div>
                   <div class="col-md-6">
                      <div class="form-group in_email">
-                        <input type="text" class="form-control" id="subject" placeholder="Subject" required="required">
+                        <input type="text" class="form-control" id="contact_subject" placeholder="Subject" required="required">
                      </div>
                   </div>
                   <div class="col-md-12">
                      <div class="form-group in_message">
-                        <textarea class="form-control" id="message" rows="5" placeholder="Message"
-                           required="required"></textarea>
+                        <textarea class="form-control" id="contact_comments" rows="5" placeholder="Message" required="required"></textarea>
                      </div>
                      <div class="actions">
-                        <input type="submit" value="Send Message" name="submit" id="submitButton" class="btn small"
-                           title="Submit Your Message!">
+                        <input type="submit" value="Send Message" name="submit" id="submit" class="btn small" title="Submit Your Message!">
                      </div>
                   </div>
                </div>
             </form>
+            <?php
+            if (isset($_POST['submit'])) {
+               $name = $_POST['contact_name'];
+               $email = $_POST['contact_email'];
+               $phone = $_POST['contact_phone'];
+               $subject = $_POST['contact_subject'];
+               $comments = $_POST['contact_comments'];
+
+               function isEmail($email)
+               {
+                  return (preg_match("/^[-_.[:alnum:]]+@((([[:alnum:]]|[[:alnum:]][[:alnum:]-]*[[:alnum:]])\.)+(ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|at|au|aw|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cs|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|in|info|int|io|iq|ir|is|it|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mil|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nt|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)$|(([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\.){3}([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5]))$/i", $email));
+               }
+
+               if (!isEmail($email)) {
+                  echo "<script type='text/javascript'> alertify.error('Attention! You have enter an invalid e-mail address, try again.');</script>";
+                  exit();
+               }
+
+               $email_to = ""; //Email message will be sent to.
+               $email_subject = 'You\'ve been contacted by ' . $first_name . ' with subject ' . $subject . '.';
+               $email_body = "You have received a new message from $name via the website contact us page.";
+               $email_content = "\"$comments\"";
+               $email_reply = "You can contact $name via email, $email or via phone $phone";
+
+               $msg = wordwrap($email_body . $email_content . $email_reply, 70);
+
+               $headers = "From: $email_to";
+               $headers .= "Reply-To: $email_to";
+               $headers .= "MIME-Version: 1.0";
+               $headers .= "Content-type: text/plain; charset=utf-8";
+               $headers .= "Content-Transfer-Encoding: quoted-printable";
+
+               if (mail($email_to, $email_subject, $msg, $headers)) {
+                  // Email has sent successfully.
+                  echo "<script type='text/javascript'> alertify.notify('Message Sent Successfully. \n Thank you <strong>' . $name . '</strong>, your message has been submitted to us', 'success', 3, function(){  console.log('dismissed'); window.location.href = 'contact.php';});</script>";
+               } else {
+                  echo "<script type='text/javascript'> alertify.error('Sorry, there was an error sending message, try again.');</script>";
+               }
+            }
+            ?>
          </div>
          <!-- <div id="googleMap" style="width:100%;height:450px;"></div> -->
-         <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.8584521564985!2d3.380028814059663!3d6.539553124792512!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8d0970575675%3A0xd011f70b0493019e!2s66%20Igi%20Olugbin%20St%2C%20Somolu%2C%20Lagos!5e0!3m2!1sen!2sng!4v1622263206887!5m2!1sen!2sng"
-            width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" loading="lazy"
-            allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.8584521564985!2d3.380028814059663!3d6.539553124792512!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8d0970575675%3A0xd011f70b0493019e!2s66%20Igi%20Olugbin%20St%2C%20Somolu%2C%20Lagos!5e0!3m2!1sen!2sng!4v1622263206887!5m2!1sen!2sng" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" loading="lazy" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
       </div>
    </div>
 
@@ -662,8 +686,7 @@
                   <div class="subcriber-box">
                      <form id="mc-form" class="mc-form">
                         <div class="newsletter-form">
-                           <input type="email" autocomplete="off" id="mc-email" placeholder="Email address"
-                              class="form-control" name="EMAIL">
+                           <input type="email" autocomplete="off" id="mc-email" placeholder="Email address" class="form-control" name="EMAIL">
                            <button class="mc-submit" type="submit"><i class="fa fa-paper-plane"></i></button>
                            <div class="clearfix"></div>
                            <!-- mailchimp-alerts Start -->
